ea1cca680ef9c8a4263c40d35ddeed252a2d0e9f
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2021-10-26 15:04:31 -0600

    added title to image gallery

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
                 $.each(result, function (i, item) {
                     console.log(i, item);
                     $.each(item.photo, function(n, pic){
-                        console.log(n, pic.id, pic.server, pic.secret);
-                        var image = "<div class='col-md-4'><img class='float-left' style='width:100%' src='https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_" + pic.secret + ".jpg'></div>"
+                        console.log(n, pic);
+                        var image = "<div class='col-md-4'><img class='float-left' style='width:100%' src='https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_" + pic.secret + ".jpg'><h4 class='text-white'>" + pic.title + "</h4></div>"
                         //$("<img>").attr("src", "https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_"+ pic.secret + ".jpg").appendTo("#gallery-div");
                         $("#gallery-div").append(image);
                     })
