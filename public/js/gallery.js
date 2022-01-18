3447a48912662dae815394ca2562e954cce05922
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-01-18 12:02:55 -0700

    added flickr api to index so we can upload special images

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -26,7 +26,39 @@ $(document).ready(function () {
             $("#gallery-div").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
             }
         });
-    })
+
+
+    });
+
+    $.get("/api", function(data){
+        console.log();
+                                                                                                                                   
+        var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=" + data.apiKey + "&user_id=194813608%40N05&format=json&nojsoncallback=1";
+        $.ajax({
+        url: flickerAPI,
+        dataType: "json", // jsonp
+        //jsonpCallback: 'jsonFlickrFeed', // add this property
+            success: function (result, status, xhr) {
+                //console.log(result);
+                $.each(result, function (i, item) {
+                    //console.log(i, item);
+                    $.each(item.photo, function(n, pic){
+                        console.log(n, pic);
+                        var image = "<div class='col-md-4'><h4 class='text-white text-center'>" + pic.title + "</h4><img class='float-left' style='width:100%' src='https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_" + pic.secret + ".jpg' loading='lazy'></div>"
+                        //$("<img>").attr("src", "https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_"+ pic.secret + ".jpg").appendTo("#gallery-div");
+                        $("#special-gallery").append(image);
+                    })
+
+                });
+        },
+            error: function (xhr, status, error) {
+            console.log(xhr, status, error)
+            $("special-gallery").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+
+
+    });
 
 
 
