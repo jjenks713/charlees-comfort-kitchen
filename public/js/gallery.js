5a2a05a50266686e9848ee3cacdf4891fd0f555f
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-12 18:54:56 -0700

    added variables in cloud

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $.get("/api", function(data){
-        console.log(data);
+        //console.log(data);
 
 
         var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=" + data.apiKey + "&user_id=194257058%40N07&format=json&nojsoncallback=1";
@@ -10,15 +10,14 @@ $(document).ready(function () {
         dataType: "json", // jsonp
         //jsonpCallback: 'jsonFlickrFeed', // add this property
             success: function (result, status, xhr) {
-                console.log(result);
+                //console.log(result);
                 $.each(result, function (i, item) {
-                    console.log(i, item);
+                    //console.log(i, item);
                     $.each(item.photo, function(n, pic){
-                        console.log(n, pic);
+                        //console.log(n, pic);
                         var image = "<div class='col-md-4'><img class='float-left' style='width:100%' src='https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_" + pic.secret + ".jpg' loading='lazy'><h4 class='text-white'>" + pic.title + "</h4></div>"
                         //$("<img>").attr("src", "https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_"+ pic.secret + ".jpg").appendTo("#gallery-div");
-                        $("#gallery-div").html(image);
-                        $("#gallery-div").html("<div>testing a bitch out</div>")
+                        $("#gallery-div").append(image);
 
                     })
 
