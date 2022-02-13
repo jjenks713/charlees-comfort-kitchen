32b1252fda4622c675c493674f3f3fd09ff6104c
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-12 17:34:19 -0700

    process

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.get("/gallery", function(req, res) {
 
 app.get("/api", function(req, res) {
 
-  var apiKey = apiKey;
-  var apiSecret = apiSecret;
+  var apiKey = process.env.apiKey;
+  var apiSecret = process.env.apiSecret;
   var apiObj = {apiKey, apiSecret};
   res.status("200").json(apiObj);
   console.log(apiObj)
