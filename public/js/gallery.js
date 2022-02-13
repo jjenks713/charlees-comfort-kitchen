ac348d7787db72e7bf7936c79b5c4ebc8dfca2fb
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-12 17:06:05 -0700

    testing with logs

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -10,11 +10,11 @@ $(document).ready(function () {
         dataType: "json", // jsonp
         //jsonpCallback: 'jsonFlickrFeed', // add this property
             success: function (result, status, xhr) {
-                //console.log(result);
+                console.log(result);
                 $.each(result, function (i, item) {
-                    //console.log(i, item);
+                    console.log(i, item);
                     $.each(item.photo, function(n, pic){
-                        //console.log(n, pic);
+                        console.log(n, pic);
                         var image = "<div class='col-md-4'><img class='float-left' style='width:100%' src='https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_" + pic.secret + ".jpg' loading='lazy'><h4 class='text-white'>" + pic.title + "</h4></div>"
                         //$("<img>").attr("src", "https://live.staticflickr.com/"+ pic.server + "/"+ pic.id + "_"+ pic.secret + ".jpg").appendTo("#gallery-div");
                         $("#gallery-div").html(image);
