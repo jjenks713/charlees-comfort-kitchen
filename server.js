5c88229ab4f95a908ab8766aee7d9d31b52212ae
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-12 17:55:25 -0700

    process log

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/api", function(req, res) {
     var apiSecret = process.env.apiSecret;
     var apiObj = {apiKey, apiSecret};
     res.status("200").json(apiObj);
-    console.log(apiObj, "this ya know")
+    console.log(apiObj, process.env)
   };
 
 
