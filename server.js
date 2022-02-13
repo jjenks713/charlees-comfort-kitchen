785bc18381d678710b22ad844a0e576147923375
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-12 18:53:51 -0700

    testing

--- server.js
+++ server.js
@@ -37,16 +37,13 @@ app.get("/gallery", function(req, res) {
     });
 
 app.get("/api", function(req, res) {
-  exports.envVar = (req, res) => {
-    // Sends 'bar' as response
-    console.log(process.env)
-
-  };
 
   var apiKey = process.env.apiKey;
   var apiSecret = process.env.apiSecret;
   var apiObj = {apiKey, apiSecret};
   res.status("200").json(apiObj);
+  console.log(apiObj, process.env)
+
 });
 
 app.post("/form", function(req, res){
